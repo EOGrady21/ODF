ec965b886613853c50d48bd32693c0e7106a0464
Chisholm <ChisholmE@ENT.dfo-mpo.ca>
2018-07-16 14:27:04 -0300

    oce2odf fix v1.3

--- odfR/R/oce2odf.R
+++ odfR/R/oce2odf.R
@@ -84,6 +84,9 @@ oce2odfHeader <- function(obj){
 #' * depthMin
 #' * depthMax
 #' * event_comments
+#' * serial_number
+#' * mooring_number
+#' * 
 #'
 #'
 #' @author E. Chisholm
@@ -345,7 +348,7 @@ oce2odf <- function(obj, write = TRUE, inst_code =NULL){
     
     
     
-     for(i in 1:length(params)){
+     
       length(b$PARAMETER_HEADER) <- length(b$PARAMETER_HEADER) + length(params)
       for (i in 1:length(params)){
         b$PARAMETER_HEADER[[i]] <- list(
@@ -386,7 +389,7 @@ oce2odf <- function(obj, write = TRUE, inst_code =NULL){
           NUMBER_NULL = length(SYTM_01) - length(na.omit(SYTM_01))
         )
       }
-    }
+    
     
     
     
