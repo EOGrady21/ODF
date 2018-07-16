f421533885c0dd6ac1799e1798c9885b2994eedd
Chisholm <ChisholmE@ENT.dfo-mpo.ca>
2018-07-16 15:01:27 -0300

    odf2oce

--- readodf2oce.R
+++ readodf2oce.R
@@ -1,27 +1,34 @@
 ####readodf2oce####
 ##using oce library
 
-#read ctd
-ctd <- read.ctd.odf(file = 'CTD_BCD2017666_01_01_DN.ODF')
+
+#' odf2oce
+#'
+#'Returns oce object with complete metadata, can also handle instruments which
+#'do not have an equivalent oce object class
+#' @param odffile an odf file name and path
+#'
+#' @return an oce object 
+#' @export
+#'
+#' @examples
+#' #read current meter
+#' #note sometimes as.cm  does not include all metadata
+#' o <- read.odf(file = 'C:/Users/ChisholmE/Documents/sample files/mcm/MCM_HUD2013008_1844_602_3600.ODF')
+#' mcm <- as.cm(o)
+#' #using odf2oce returns all metadata from odf
+#' mcm <- oce2odf('C:/Users/ChisholmE/Documents/sample files/mcm/MCM_HUD2013008_1844_602_3600.ODF')
 
 
-#read current meter
-o <- read.odf(file = 'mcm/MCM_HUD2013008_1844_602_3600.ODF')
-mcm <- as.cm(o)
+odf2oce <- function(odffile){
+  odf <- read.odf(file = odffile)
+  oce <- new('oce')
+  oce@data <- odf@data
+  oce@metadata <- odf@metadata
+  return(oce)
+}
 
 
 
 
-#read any object without oce class
-d <- read.odf(file = 'mtr/MTR_HUD2015030_1898_10546422_1800.ODF')
-oce <- new('oce')
-oce@data <- d@data
-oce@metadata <- d@metadata
-
-
-
-f <- read.odf(file = 'C:/Users/ChisholmE/Documents/sample files/mctd/MCTD_HUD2015006_1897_11688_1800.ODF')
-mctd <- new('oce')
-mctd@data <- f@data
-mctd@metadata <- f@metadata
 
